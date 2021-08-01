602ce85d39682ebc06c617ff8ecb12ad9e494d69
earsuo <1391805182@qq.com>
2021-08-01 14:50:54 +0800

    增加个人信息编辑页面

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,9 @@
 export default {
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ["pages/index/index", "pages/personalEdit/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black"
   }
-}
+};
