a7309651f5b2d1e2360a5e968c23b41ae0489350
earsuo <1391805182@qq.com>
2021-09-11 21:06:48 +0800

    up

--- src/pages/login/components/agreement.tsx
+++ src/pages/login/components/agreement.tsx
@@ -1,14 +1,31 @@
 import { Component } from "react";
 import { View, Text, Image, ScrollView, Radio } from "@tarojs/components";
+import {
+  AtButton,
+  AtList,
+  AtListItem,
+  AtTabs,
+  AtTabsPane,
+  AtToast
+} from "taro-ui";
 import "../index.less";
 
-class Agreement extends Component {
+class Agreement extends Component<any, any> {
   constructor(props) {
     super(props);
   }
 
+  back = () => {
+    this.props.toggle(false);
+  };
+
   render() {
-    return <View className="agreement_wrap">24234</View>;
+    return (
+      <View className="agreement_wrap">
+        24234
+        <AtButton onClick={this.back}>返回</AtButton>
+      </View>
+    );
   }
 }
 
